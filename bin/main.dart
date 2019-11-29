7f865f0176dcfc40efa460c9fa8cbc5e812ea97f
Sahil Singh <sahil.dogstar@gmail.com>
2019-11-29 11:31:42 +0530

    Day 41 morning challenge completed.

--- bin/main.dart
+++ bin/main.dart
@@ -9,5 +9,143 @@
 
 
 // Stretch: Show all moves which lead to win, given a state.
+
+
+List<List<int>> willWinInOneMove(List<List<int>> inputState) {
+  List<List<List<int>>> allPossibleMoves = possibleMoves(inputState);
+  for (int i = 0; i < allPossibleMoves.length - 1; i++) {
+      if(checkForFinalState(allPossibleMoves[i])){
+        return allPossibleMoves[i];
+      }
+          
+  }
+}
+
+List<List<int>> willWinInTwoMoves(List<List<int>> inputState){
+  List<List<List<int>>> allPossibleMoves = possibleMoves(inputState);
+  for(List<List<int>> list in allPossibleMoves){
+    if(willWinInOneMove(list) != null){
+      return list;
+    }
+  }
+  return null;
+}
+
+List<List<int>> willWinInThreeMoves(List<List<int>> inputState){
+  List<List<List<int>>> allPossibleMoves = possibleMoves(inputState);
+  for(List<List<int>> list in allPossibleMoves){
+    if(willWinInTwoMoves(list) != null){
+      return list;
+
+    }
+  }
+  return null;
+}
+
+List<List<int>> willWinInGivenNumberOfMoves(List<List<int>> inputState, int numberOfMoves){
+  List<List<List<int>>> allPossibleMoves = possibleMoves(inputState);
+  if(numberOfMoves == 1){
+    return willWinInOneMove(inputState);
+  }
+  for(List<List<int>> list in allPossibleMoves){
+    if(willWinInGivenNumberOfMoves(list,  numberOfMoves-1)!=null){
+      return list;
+    }
+  }
+    
+  
+  return null;
+}
+
+bool checkForFinalState(List<List<int>> listToCheck){
+  if(listToCheck[0].isEmpty && listToCheck[1].isEmpty){
+  return true;
+  }
+  return false;
+}
+
+List<List<List<int>>> possibleMoves(List<List<int>> inputState) {
+  List<List<List<int>>> finalState = [];
+
+  for (int i = 0; i < inputState.length; i++) {
+    for (int j = 0; j < inputState.length; j++) {
+      if (isValidMove(inputState, i, j)) {
+        List<List<int>> copyInputState = [];
+        for (List<int> list in inputState) {
+          List<int> tempList = List.from(list);
+          copyInputState.add(tempList);
+        }
+        copyInputState[j].insert(0, copyInputState[i][0]);
+
+        copyInputState[i].removeAt(0);
+
+        finalState.add(copyInputState);
+      }
+    }
+  }
+  return finalState;
+}
+
+bool isValidMove(List<List<int>> checkList, int from, int to) {
+  if (checkList[from].isEmpty) {
+    return false;
+  }
+
+  if (checkList[to].isEmpty) {
+    return true;
+  }
+
+  return (checkList[from].first < checkList[to].first);
+}
+
+
+
+
+void printPossibleMoves(List<List<int>> inputState, int numberOfMoves){
+
+  while(numberOfMoves > 0){
+   List<List<int>> nextMove = willWinInGivenNumberOfMoves(inputState, numberOfMoves);
+   print(nextMove);
+   inputState = nextMove;
+   numberOfMoves = numberOfMoves - 1;
+  }
+    
+  
+ 
+
+}
+
 main() {
+  
+
+
+
+  /*print(possibleMoves([
+    [1],
+    [],
+    [2, 3, 4]
+  ]));
+   print(willWinInTwoMoves([
+    [1],
+    [2],
+    [3, 4]
+  ]));
+  print(willWinInOneMove([
+    [1],
+    [],
+    [2, 3, 4]
+  ]));*/
+  // print(secondBestMove([[1],[2],[3,4]]));
+  // print(willFinishInTwoMoves([[2],[1],[3,4]]));
+  //print(possibleMoves([[2,3],[1],[4]]));
+  //print(bestMoves([[2,3], [1], [4,]]));
+  /*print(willWinInThreeMoves([[2,1],[3],[4]]));
+  print(possibleMoves([[1,2],[3],[4]]));*/
+ // print(willWinInGivenNumberOfMoves([[1,2],[],[3,4]], 3));
+  //print(possibleMoves([[1,2],[],[3,4]]));
+  printPossibleMoves([[2,1],[3],[4]], 3);
+
+  
+
+
 }
